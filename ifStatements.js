e7f48e4cba07a3db488a14d7808338659917af48
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-04 01:17:46 -0600

    Add ifStatementquestsions complete

--- ifStatements.js
+++ ifStatements.js
@@ -4,7 +4,7 @@
 // (while navigated to the codeChallenges directory):
 // `node ifStatements.js`
 
-// Example: Using the weather variable below, write code that decides 
+// Example: Using the weather variable below, write code that decides
 // what you should take with you based on the following conditions:
   // if it is sunny, print "sunscreen"
   // if it is rainy, print "umbrella"
@@ -27,7 +27,8 @@ if (weather === "sunny") {
 
 // Experiment with manipulating the value held in variable 'weather'
 // to print something other than 'coat'
-
+weather = "rainy"
+umbrella
 
 ##################
 // Using the numQuarters variable defined below, determine
@@ -36,21 +37,22 @@ if (weather === "sunny") {
 
 // Right now, the program will print
 // out both "I have enough money for a gumball" and
-// "I don't have enough money for a gumball". Write a 
+// "I don't have enough money for a gumball". Write a
 // conditional statement that prints only one or the other.
 
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
 numQuarters = 0
-
+if (numQuarters >= 1)
 console.log("I have enough money for a gumball");
 console.log("I don't have enough money for a gumball");
 
+// I don't have enough money for a gumball
 
 // #####################
 // Using the variables defined below, write code that will tell you
-// if you have the ingredients to make a pizza. A pizza requires 
+// if you have the ingredients to make a pizza. A pizza requires
 // at least two cups of flour and sauce.
 
 // You should be able to change the variables to achieve the following outconsole.log();
@@ -64,4 +66,3 @@ console.log("I don't have enough money for a gumball");
 
 var cupsOfFlour = 1;
 var hasSauce = true;
-
